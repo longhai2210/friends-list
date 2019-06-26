b8b5b32a0e4a4661c548cbd9eaedb418e46dc0ec
qe <qe@qes-Mac-mini.local>
2019-06-26 16:56:47 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/women/${img}.jpg`;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="person">
+    <img src={url} alt="" />
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
     </div>
+  </div>);
+}
+
+const PersonList = () => {
+  const people = [
+    {img:60, name:'dianna', job:'developer'},
+    {img:49, name:'sue', job:'designer'},
+    {img:58, name:'libra', job:'fashionista'}
+  ]
+  
+  return (<section>
+    <Person person={people[0]} />
+    <Person person={people[1]}>
+      This person description   
+    </Person> 
+    <Person person={people[2]} />
+  </section>  
   );
 }
+
+const App = () => (<PersonList />)
 
 export default App;
